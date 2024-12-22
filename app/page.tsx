6034bb37a327e7d5d2957865dba1b291680dbf34
Tahasiraj1 <tahasiraj242@gmail.com>
2024-12-22 18:48:15 +0500

    Added WhyUs.tsx component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Founder from "@/components/Founder";
 import Hero from "@/components/Hero";
 import Programs from '@/components/Programs'
 import ProgramsSection from '@/components/ProgramsSection'
-// import WhyUs from "@/components/WhyUs";
+import WhyUs from "@/components/WhyUs";
 
 export default function Home() {
   return (
     <div className="items-center justify-items-center min-h-screen">
       <Hero />
-      {/* <WhyUs /> */}
+      <WhyUs />
       <Programs />
       <ProgramsSection />
       <Founder />
--- components/WhyUs.tsx
+++ components/WhyUs.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { SlantedHeading } from './layout/SlantedHeading'
+import { FaRegCircleXmark } from "react-icons/fa6";
+import { LuCircleCheckBig } from "react-icons/lu";
+
 
 const WhyUs = () => {
   return (
@@ -10,22 +13,24 @@ const WhyUs = () => {
         width='w-full'
         />
         <div className='flex flex-col lg:flex-row gap-6 py-10 px-10'>
-            <div className='flex flex-col w-[500px] h-full bg-red-700 items-center justify-center px-5 py-5'>
-                <ul className='space-y-4 text-start'>
-                    <li>Self Defense Focused</li>
-                    <li>Safety First</li>
-                    <li>Curriculum Based</li>
-                    <li>Flexible Class Schedule</li>
-                    <li>No-Contract Memberships</li>
+            <div className='flex flex-col w-[400px] h-full rounded-md border-2 border-red-700 px-5 py-5'>
+                <h2 className='text-xl font-semibold text-center mb-4'>Our Training</h2>
+                <ul className='space-y-2'>
+                    <li className='flex items-center gap-3'><LuCircleCheckBig className='text-red-500 w-5 h-5' />Self Defense Focused</li>
+                    <li className='flex items-center gap-3'><LuCircleCheckBig className='text-red-500 w-5 h-5' />Safety First</li>
+                    <li className='flex items-center gap-3'><LuCircleCheckBig className='text-red-500 w-5 h-5' />Curriculum Based</li>
+                    <li className='flex items-center gap-3'><LuCircleCheckBig className='text-red-500 w-5 h-5' />Flexible Class Schedule</li>
+                    <li className='flex items-center gap-3'><LuCircleCheckBig className='text-red-500 w-5 h-5' />No-Contract Memberships</li>
                 </ul>
             </div>
-            <div className='flex flex-col w-[500px] h-full bg-red-700 items-center justify-center px-5 py-5'>
-            <ul className='space-y-4'>
-                <li>Self Defense Focused</li>
-                <li>Safety First</li>
-                <li>Curriculum Based</li>
-                <li>Flexible Class Schedule</li>
-                <li>No-Contract Memberships</li>
+            <div className='flex flex-col w-[400px] h-full rounded-md border-2 border-red-700 px-5 py-5'>
+                <h2 className='text-xl font-semibold text-center mb-4'>Our Training</h2>
+                <ul className='space-y-2'>
+                    <li className='flex items-center gap-3'><FaRegCircleXmark className='text-red-500 w-5 h-5' />Self Defense Focused</li>
+                    <li className='flex items-center gap-3'><FaRegCircleXmark className='text-red-500 w-5 h-5' />Safety First</li>
+                    <li className='flex items-center gap-3'><FaRegCircleXmark className='text-red-500 w-5 h-5' />Curriculum Based</li>
+                    <li className='flex items-center gap-3'><FaRegCircleXmark className='text-red-500 w-5 h-5' />Flexible Class Schedule</li>
+                    <li className='flex items-center gap-3'><FaRegCircleXmark className='text-red-500 w-5 h-5' />No-Contract Memberships</li>
                 </ul>
             </div>
         </div>
