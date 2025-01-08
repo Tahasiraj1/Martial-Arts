a61c46138a8e8f04496d43a2ebadf65854f0500d
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-09 00:19:41 +0500

    Refactor Prisma client import to instantiate PrismaClient directly in admin and student API routes

--- app/api/admin/students/route.ts
+++ app/api/admin/students/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
-import prisma from '@/lib/prisma'
+import { PrismaClient } from "@prisma/client";
 import { auth, clerkClient } from "@clerk/nextjs/server";
+
+const prisma = new PrismaClient();
 
 
 async function isAdmin(userId: string) {
--- app/api/student/route.ts
+++ app/api/student/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
-import prisma from '@/lib/prisma'
+import { PrismaClient } from "@prisma/client";
 import { auth } from "@clerk/nextjs/server";
+
+const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
