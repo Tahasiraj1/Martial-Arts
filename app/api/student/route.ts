5343abe1922020db87f1241dee2137dd66165ef8
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-08 23:57:28 +0500

    Refactor Prisma client import to use a centralized instance in admin and student API routes

--- app/api/student/route.ts
+++ app/api/student/route.ts
@@ -1,14 +1,16 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import prisma from '@/lib/prisma'
 import { auth } from "@clerk/nextjs/server";
 
-const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
     const { userId } = await auth();
     if (!userId) {
-      return NextResponse.json({ error: "Sign in to register" }, { status: 401 });
+      return NextResponse.json(
+        { error: "Sign in to register" },
+        { status: 401 }
+      );
     }
 
     const data = await req.json();
@@ -46,19 +48,15 @@ export async function GET() {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-      const student = await prisma.student.findUnique({
-        where: { clerkId: userId },
-      });
+    const student = await prisma.student.findUnique({
+      where: { clerkId: userId },
+    });
 
-      if (!student) {
-        return NextResponse.json(
-          { error: "Student not found" },
-          { status: 404 }
-        );
-      }
-      
-      return NextResponse.json({ isAdmin: false, student });
+    if (!student) {
+      return NextResponse.json({ error: "Student not found" }, { status: 404 });
+    }
 
+    return NextResponse.json({ isAdmin: false, student });
   } catch (error) {
     console.error("Error fetching student:", error);
     return NextResponse.json(
@@ -67,4 +65,3 @@ export async function GET() {
     );
   }
 }
-
